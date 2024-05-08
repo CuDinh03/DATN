8920c397a6cc49ce8931ec54dba92c854dbe9979
CuDinh03 <dinhhuycu0305@gmail.com>
2024-05-08 11:55:36 +0700

    add webconfig cors 4200

--- DATN/src/main/java/fpl/but/datn/controller/UserController.java
+++ DATN/src/main/java/fpl/but/datn/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/users")
 @Slf4j
