2d8f6859ccdca2e510481dfe315c82ca9eb0e42a
CuDinh03 <dinhhuycu0305@gmail.com>
2024-05-08 11:56:32 +0700

    update cors 4200

--- DATN/src/main/java/fpl/but/datn/configuration/WebConfig.java
+++ DATN/src/main/java/fpl/but/datn/configuration/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
