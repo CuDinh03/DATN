3c0c57fdc0acbb2f989f547e718ebd6024dd5e73
CuDinh03 <dinhhuycu0305@gmail.com>
2024-06-03 13:41:59 +0700

    update

--- DATN/src/main/java/fpl/but/datn/repository/GioHangChiTietRepository.java
+++ DATN/src/main/java/fpl/but/datn/repository/GioHangChiTietRepository.java
@@ -2,9 +2,6 @@ package fpl.but.datn.repository;
 
 
 import fpl.but.datn.entity.GioHangChiTiet;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
