9968bf6f7818d48ff09aa3d2b4a301ffbcaf19ed
sangdhph28599 <118247908+ph28599@users.noreply.github.com>
2024-06-15 01:17:27 +0700

    update chat lieu

--- DATN/src/main/java/fpl/but/datn/repository/ChatLieuRepository.java
+++ DATN/src/main/java/fpl/but/datn/repository/ChatLieuRepository.java
@@ -1,13 +1,19 @@
 package fpl.but.datn.repository;
 
 import fpl.but.datn.entity.ChatLieu;
+import fpl.but.datn.entity.SanPham;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface ChatLieuRepository extends JpaRepository<ChatLieu, UUID> {
+    @Query("SELECT cl FROM ChatLieu cl ORDER BY cl.ngayTao DESC")
+    Page<ChatLieu> findAll(Pageable pageable);
 
     Optional<ChatLieu> findByMa(String ma);
     boolean existsByMa(String ma);
--- DATN/src/main/java/fpl/but/datn/service/IChatLieuService.java
+++ DATN/src/main/java/fpl/but/datn/service/IChatLieuService.java
@@ -1,6 +1,9 @@
 package fpl.but.datn.service;
 
 import fpl.but.datn.entity.ChatLieu;
+import fpl.but.datn.entity.SanPham;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,6 +12,9 @@ public interface IChatLieuService {
     List getAll();
     ChatLieu create(ChatLieu chatLieu);
     ChatLieu update(ChatLieu chatLieu, UUID id);
-    boolean delete(UUID id);
+    void delete(UUID id);
+    void open(UUID id);
+
     ChatLieu findById(UUID id);
+    Page<ChatLieu> getAllChatLieuPageable(Pageable pageable);
 }
